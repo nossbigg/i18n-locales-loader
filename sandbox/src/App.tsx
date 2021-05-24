e6a4dcb901c1cd960eae7775e20c08eea5ef914f
nossbigg <chenggibson@gmail.com>
2021-05-24 12:36:12 +0800

    Add sandbox nested route setup

--- sandbox/src/App.tsx
+++ sandbox/src/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { MyPage } from "./MyPage";
 
 function App() {
-  return <MyPage />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={MyPage} />
+        <Route path="/path1" component={MyPage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
