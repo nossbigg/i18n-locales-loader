ecdd4efdbd8ed189dc26bb5a29296326a1e76d57
nossbigg <chenggibson@gmail.com>
2021-05-26 04:36:03 +0800

    Add task to view code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 const config = {
   testPathIgnorePatterns: ["/node_modules/", "<rootDir>/sandbox"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.js", "!**/node_modules/**"],
+  coverageReporters: ["text"],
 };
 
 module.exports = config;
